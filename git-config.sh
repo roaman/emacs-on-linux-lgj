075e42b785316b7df1a6d9160031204ba97d60d7
roaman <liguojun518@gmail.com>
2016-12-12 17:07:39 +0800

    modify pre-setup-emacs.sh name

--- git-config.sh
+++ git-config.sh
@@ -1,3 +1,3 @@
-git config --global user.name "LiGuojun"
-git config --global user.email liguojun@hi1918.com
+git config --global user.name "roaman"
+git config --global user.email liguojun518@gmail.com
 git config --global core.editor 'emacs -q'
