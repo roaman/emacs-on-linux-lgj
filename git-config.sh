5cd939a3e453368a14685aef25885557f162b657
roaman <liguojun518@gmail.com>
2016-12-23 22:08:00 +0800

    enable rerere

--- git-config.sh
+++ git-config.sh
@@ -2,3 +2,4 @@ git config --global user.name "roaman"
 git config --global user.email liguojun518@gmail.com
 git config --global core.editor 'emacs -q'
 git config --global credical
+git config --global rerere.enabled true
